33cdf7a93222b27b6caf1c9511461c1d16982fb5
qameta-ci <qameta-ci@qameta.io>
2024-10-01 11:01:38 +0000

    update 2024-10-01T11:01:38

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    setupFiles: ["./vitest.setup.js"],
+    setupFiles: ["allure-vitest/setup"],
     reporters: [
       "default",
       ["allure-vitest/reporter", { resultsDir: "./out/allure-results" }],
